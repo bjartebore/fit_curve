23c5622e4a54647091a3cd14ea92b6de336729ae
Bjarte bore <bjarte.bore@olavstoppen.no>
2021-07-09 10:50:35 +0200

    added a Curve.fromList

--- lib/src/curve.dart
+++ lib/src/curve.dart
@@ -2,6 +2,17 @@ import 'dart:math';
 
 class Curve {
   Curve(this.point1, this.handle1, this.handle2, this.point2);
+
+  factory Curve.fromList(List<List<double>> list) {
+    assert(list.length == 4, 'list should be no more or no less then the length of 4');
+    return Curve(
+      Point(list[0][0], list[0][1]),
+      Point(list[1][0], list[1][1]),
+      Point(list[2][0], list[2][1]),
+      Point(list[3][0], list[3][1]),
+    );
+  }
+
   // first point
   Point<double> point1;
   // first controlpoint
